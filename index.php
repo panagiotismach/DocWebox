aea263e1df01db9f46cd1cbf8e8e3a037a09756d
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2022-11-05 13:29:26 +0200

    Nav creation and page additions

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" , initial-scale="1.0" />
+    <title>DocWebox - Find your Doctor!</title>
+    <link rel="stylesheet" href="" />
+    <script src="" defer></script>
+    <link rel="icon" href="./public/resources/favicon/icon.ico" />
+  </head>
+  <body>
+    <header>
+      <nav>
+        <img src="./public/resources/logos/main-logo-transparent.png" alt="DocWebox logo" class="nav__logo" id="logo" />
+        <ul>
+          <li>
+            <a href="/public/views/news.php">News</a>
+          </li>
+          <li>
+            <a href="/public/views/contact.php">Contact</a>
+          </li>
+          <li>
+            <a href="/public/views/sign-up.php">Sign up</a>
+          </li>
+          <li>
+            <a href="/public/views/login.php">Login</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    </body>
+</html>
