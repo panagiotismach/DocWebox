f26c376d62e3e61364cd3950ab072507bb1104c4
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2022-11-05 15:22:42 +0200

    First implementation of the navigation bar

--- index.php
+++ index.php
@@ -4,17 +4,23 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" , initial-scale="1.0" />
     <title>DocWebox - Find your Doctor!</title>
-    <link rel="stylesheet" href="" />
+    <link rel="stylesheet" href="./public/styles/index-styling.css" />
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
     <script src="" defer></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="icon" href="./public/resources/favicon/icon.ico" />
   </head>
   <body>
     <header>
       <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
         <img src="./public/resources/logos/main-logo-transparent.png" alt="DocWebox logo" class="nav__logo" id="logo" />
         <ul>
           <li>
-            <a href="/public/views/news.php">News</a>
+            <a class="active" href="/public/views/news.php">News</a>
           </li>
           <li>
             <a href="/public/views/contact.php">Contact</a>
