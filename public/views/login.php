eb6eb35523fecee1c76a24d80df53023109347c2
Panagiotis Machairas <ics20044@uom.edu.gr>
2022-11-06 22:59:15 +0200

    Create login form. Additions: -login user -login style

---
+++ public/views/login.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+    <head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="../login-register.css">
+    <title>Login Page</title>
+    </head>
+    <body>
+
+    <section class="my-4 mx-5">
+        <div class="container">
+          <div class="row no-gutters">
+            <div class="col-lg-6 section-img">
+              <img src="../resources/logos/main-logo-transparent.png" class="mx-auto d-block">
+              <h4 class="header">Hi</h4>
+            </div>
+        
+            <div class="col-lg-6 px-5 pt-5 ">
+              <form>
+                <h4 class="font-weigth-bold header">Weclome back</h4>
+                <div class="">
+                  <div class="col-lg-7">
+                    <label class="form-label">Username</label>
+                    <input type="text" class="form-control my-2 p-2" placeholder="username">
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="col-lg-7">
+                    <label class="form-label">Password</label>
+                    <input type="password" class="form-control my-2 p-2" placeholder="password">
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="col-lg-7">
+                      <button type="button" class="btn btn-primary btns my-2 p-2 ">Login</button>
+                  </div>
+                </div>
+                <div class="d-flex justify-content-around">
+                    <div class="d-flex align-items-center">
+                     <hr class="">
+                    </div>
+                    
+                    <p >Or</p>
+                    <div class="d-flex align-items-center">
+                      <hr>
+                    </div>
+                    
+                  </div>
+                <div class="form-row">
+                  <div class="col-lg-7">
+                      <button type="button" class="btn btn-primary btns">Register</button>
+                  </div>
+                      
+                </div>
+                    
+                
+        </div>
+      
+        </section>
+ 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+</html>
