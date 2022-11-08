1456919a5e8b0cc9d653c6ba83d405a76a8e9cfc
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2022-11-08 19:16:04 +0200

    Addition of the header and the footer on all login-register pages

--- public/views/register-patient.php
+++ public/views/register-patient.php
@@ -4,20 +4,25 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
       <link rel="stylesheet" href="../styles/login-register.css">
-      <title>Register Page Patient</title>
+      <title>DocWebox - Find your Doctor!</title>
     </head>
     <body>
+    <header>
+      <nav>
+        <a href="/DocWebox/index.php"><img src="../resources/logos/logo-back-transparent.png" alt="DocWebox logo back" class="nav__logo" id="logo" /></a>
+      </nav>
+    </header>
     <section class="my-5 mx-5">
         <div class="container">
           <div class="row no-gutters">
             <div class="col-lg-6 section-img" id="patient-c">
               <img src="../resources/logos/main-logo-transparent.png" class="mx-auto d-block">
-              <h4 class="header">Welcome to DocWebox</h4>
-              <a href="../../../DocWebox/index.php" id="back-home-patient" class="d-flex justify-content-center align-self-end">Back to home page</a>
+              <!-- <h4 class="header">Welcome to DocWebox</h4>
+              <a href="../../../DocWebox/index.php" id="back-home-patient" class="d-flex justify-content-center align-self-end">Back to home page</a> -->
           </div>
           <div class="col-lg-6 section-form">
               <form class="">
-                <h4 class="font-weigth-bold header">Welcome. Register as a Patient and become a member</h4>
+                <h4 class="font-weigth-bold header">Glad to have you on board!</h4>
                 <div class="d-flex justify-content-center align-items-center">
                   <div class="col-lg-7">
                     <label class="form-label">Firstname</label>
@@ -57,7 +62,7 @@
                     <div class="d-flex align-items-center">
                      <hr>
                     </div>
-                    <p >Or</p>
+                    <p >Already registered?</p>
                     <div class="d-flex align-items-center">
                       <hr>
                     </div>
@@ -69,6 +74,13 @@
               </div>
             </div>
        </section>
+       <footer class="footer">
+      <img src="../resources/logos/main-logo-transparent.png" alt="Logo" class="footer__logo" />
+      <p class="footer__copyright">
+        &copy; Project owned by 
+        <a class="footer__link" target="_blank" href="https://github.com/ics20044/DocWebox">Us</a>.
+      </p>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </body>
 </html>
