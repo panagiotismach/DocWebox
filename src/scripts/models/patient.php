fd0b0a59a90cf608ccc0738dfd73a3caf49873fc
Panagiotis Machairas <ics20044@uom.edu.gr>
2022-11-09 21:40:02 +0200

    Create patient model

--- src/scripts/models/patient.php
+++ src/scripts/models/patient.php
@@ -5,12 +5,9 @@
 // `username` varchar(20) NOT NULL,
 // `email` varchar(255) NOT NULL,
 // `password` varchar(50) NOT NULL,
-// `specialization` varchar(255) NOT NULL,
-// `vat` varchar(40) NOT NULL,
-// `location` varchar(255) NOT NULL
 // `image` varchar(255) NOT NULL
 
-class Doctor {
+class Patient {
 
     public $id;
     public $firstname;
@@ -18,21 +15,15 @@ class Doctor {
     public $username;
     public $email;
     public $password;
-    public $specialization;
-    public $vat;
-    public $location;
     public $image;
 
-    public function __construct($id, $firstname, $lastname, $username, $email, $password, $specialization, $vat, $location, $image){
+    public function __construct($id, $firstname, $lastname, $username, $email, $password, $image){
         $this->id = $id;
         $this->firstname = $teamfirstnameid;
         $this->lastname = $lastname;
         $this->username = $username;
         $this->email = $email;
         $this->password = $password;
-        $this->specialization = $specialization;
-        $this->vat = $vat;
-        $this->location = $location;
         $this->image = $image;
     }
 
