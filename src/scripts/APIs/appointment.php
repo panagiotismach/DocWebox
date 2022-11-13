390426d517bc3670daaa63e749d801b867655420
Panagiotis Machairas <ics20044@uom.edu.gr>
2022-11-13 12:49:21 +0200

    Call the apis for appointment

--- src/scripts/APIs/appointment.php
+++ src/scripts/APIs/appointment.php
@@ -3,21 +3,28 @@
     require_once "../../db/connect.php";
     include_once "../../scripts/services/appointmentService.php";
 
+    include "../../scripts/utils/validation-data.php";
+
     $mysqli->select_db("docwebox");
     $appointmentService = new AppointmentService("appointment", $mysqli);
 
     if($_SERVER['REQUEST_METHOD'] == "GET"){
         $data = null;
-        
-        $patient_id = $_GET["patient_id"];
 
-        
-        $data = $appointmentService->findDoctorAppointments($patient_id);
-        
+        if(isset($_GET["doctor_id"])){
+            $doctor_id = validate_data($_GET['doctor_id']);
+            $data = $appointmentService->findDoctorAppointments($doctor_id);
+        }
 
+        if(isset($_GET["patient_id"])){
+            $patient_id = validate_data($_GET['patient_id']);
+            $data = $appointmentService->findPatientAppointments($patient_id);       
+        }
         
         header("Content-Type: application/json");
+        
         echo json_encode($data);
+
 
     }
 
