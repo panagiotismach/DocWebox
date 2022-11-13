90c4bca36f073261e672d3183a8c73047a081160
Panagiotis Machairas <ics20044@uom.edu.gr>
2022-11-13 12:37:51 +0200

    Complete the calls of patient apis

--- src/scripts/APIs/patient.php
+++ src/scripts/APIs/patient.php
@@ -1,19 +1,33 @@
 <?php
     require_once "../../db/connect.php";
     include_once "../../scripts/services/patientService.php";
+
+    include "../../scripts/utils/validation-data.php";
+
     $mysqli->select_db("docwebox");
     $patientService = new PatientService("patient", $mysqli);
 
     if($_SERVER['REQUEST_METHOD'] == "GET"){
         $data = null;
 
-        $lastname = $_GET["lastname"];
+        if(isset($_GET["id"])){
+            $id = validate_data($_GET['id']);
+            $data = $patientService->findPatientById($id);
+        }
 
-        if($lastname){
+        if(isset($_GET["firstname"])){
+            $firstname = validate_data($_GET['firstname']);
+            $data = $patientService->findPatientByFirstname($firstname);       
+        }
+
+        if(isset($_GET["lastname"])){
+            $lastname = validate_data($_GET['lastname']);
             $data = $patientService->findPatientByLastname($lastname);
         }
 
+        
         header("Content-Type: application/json");
+        
         echo json_encode($data);
     }
 
