39aeba09601d02f6a066d121106d327aa769c3d3
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2022-11-15 18:33:27 +0200

    Edit Patient and Edit Appointment modals complete

--- public/src/js/edit-modal-controller.js
+++ public/src/js/edit-modal-controller.js
@@ -1,18 +1,44 @@
-const modal = document.querySelector(".edit-modal");
-const trigger = document.querySelector(".edit-modal-trigger");
-const closeButton = document.querySelector(".edit-modal-close-button");
+const modalDoctor = document.querySelector(".edit-modal-doctor");
+const modalPatient = document.querySelector(".edit-modal-patient");
+const modalAppointment = document.querySelector(".edit-modal-appointment");
+const triggerD = document.querySelector(".edit-modal-trigger-doctor");
+const triggerP = document.querySelector(".edit-modal-trigger-patient");
+const triggerA = document.querySelector(".edit-modal-trigger-appointment");
+const closeButtonD = document.querySelector(".edit-modal-close-button-doctor");
+const closeButtonP = document.querySelector(".edit-modal-close-button-patient");
+const closeButtonA = document.querySelector(".edit-modal-close-button-appointment");
 
-function toggleModal() {
-  modal.classList.toggle("show-modal");
+function toggleModalDoctor() {
+  modalDoctor.classList.toggle("show-modal");
+}
+function toggleModalPatient() {
+  modalPatient.classList.toggle("show-modal");
+}
+function toggleModalAppointment() {
+  modalAppointment.classList.toggle("show-modal");
 }
 
 function windowOnClick(event) {
-  if (event.target === modal) {
-    toggleModal();
-    console.log("second");
+  if (event.target === modalDoctor) {
+    toggleModalDoctor();
+  } else if (event.target === modalPatient) {
+    toggleModalPatient();
+  } else if (event.target === modalAppointment) {
+    toggleModalAppointment();
   }
 }
 
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
+//Doctor modal
+triggerD.addEventListener("click", toggleModalDoctor);
+closeButtonD.addEventListener("click", toggleModalDoctor);
+window.addEventListener("click", windowOnClick);
+
+//Patient modal
+triggerP.addEventListener("click", toggleModalPatient);
+closeButtonP.addEventListener("click", toggleModalPatient);
+window.addEventListener("click", windowOnClick);
+
+//Appointment modal
+triggerA.addEventListener("click", toggleModalAppointment);
+closeButtonA.addEventListener("click", toggleModalAppointment);
 window.addEventListener("click", windowOnClick);
--- public/views/admin-views/admin-dashboard.php
+++ public/views/admin-views/admin-dashboard.php
@@ -42,20 +42,39 @@
     <div class="card-container">
             <div class="card">
              <div class="data">
-              <h3>{{Doctor Name}}</h3>
-              <p>{{Doctor Phone}} {{Doctor Address}}</p>
+              <h3>{{Doctor Full name}}</h3>
               </div>
               <ul class="buttonpd">
-                <li><button class="edit-modal-trigger" id="edit-btn">Edit</button></li>
+                <li><button class="edit-modal-trigger-doctor" id="edit-btn">Edit</button></li>
+                <li><button id="delete-btn" onclick="return confirm('⚠ Deleting is permanent and cannot be reversed')">Delete</button></li>
+              </ul>
+            </div>
+            <div class="card">
+             <div class="data">
+              <h3>{{Patient Full name}}</h3>
+              </div>
+              <ul class="buttonpd">
+                <li><button class="edit-modal-trigger-patient" id="edit-btn">Edit</button></li>
+                <li><button id="delete-btn" onclick="return confirm('⚠ Deleting is permanent and cannot be reversed')">Delete</button></li>
+              </ul>
+            </div>
+            <div class="card">
+             <div class="data">
+              <h3>{{Appointment Date}}</h3>
+              <p>{{Appointment hour}}</p>
+              <p>{{Doctor Full name}} {{Patient Fullname}}</p>
+              </div>
+              <ul class="buttonpd">
+                <li><button class="edit-modal-trigger-appointment" id="edit-btn">Edit</button></li>
                 <li><button id="delete-btn" onclick="return confirm('⚠ Deleting is permanent and cannot be reversed')">Delete</button></li>
               </ul>
             </div>
       </div>
     </div>
    </div>
-<div class="edit-modal">
+<div class="edit-modal-doctor">
     <div class="edit-modal-content">
-        <span class="edit-modal-close-button">×</span>
+        <span class="edit-modal-close-button-doctor">×</span>
         <form class="admin-edit-form">
         <i class="fa-solid fa-file-pen"></i>
         <h2>Currently editing {{Doctor Name}}</h2>
@@ -107,6 +126,85 @@
                     <div class="inputBox">
                         <span>Password</span><br>
                         <input type="text" name="Password">
+                    </div>
+                    <div class="inputBox">
+                        <input type="submit" name="Submit" value="Confirm changes">
+                    </div>
+                </form>
+    </div>
+</div>
+<div class="edit-modal-patient">
+    <div class="edit-modal-content">
+        <span class="edit-modal-close-button-patient">×</span>
+        <form class="admin-edit-form">
+        <i class="fa-solid fa-file-pen"></i>
+        <h2>Currently editing {{Patient Name}}</h2>
+                    <div class="inputBox">
+                        <span>First name</span><br>
+                        <input type="text" name="Firstname" value="{{Patient's first name}}">
+                    </div>
+                    <div class="inputBox">
+                        <span>Last name</span><br>
+                        <input type="text" name="Lastname" value="{{Patient's last name}}">
+                    </div>
+                    <div class="inputBox">
+                        <span>Phone</span><br>
+                        <input type="text" name="Phone" value="{{Patient's phone}}">
+                    </div>
+                    <div class="inputBox">
+                        <span>Location</span><br>
+                        <input type="text" name="Location" value="{{Patient's location}}">
+                    </div>
+                    <div class="inputBox">
+                        <span>Profile Picture</span><br>
+                        <input type="file" name="Profile Picture">
+                    </div>
+                    <h4>⚠ Danger Zone</h4>
+                    <div class="inputBox">
+                        <span>Username</span><br>
+                        <input type="text" name="Username">
+                    </div>
+                    <div class="inputBox">
+                        <span>Email</span><br>
+                        <input type="text" name="Email">
+                    </div>
+                    <div class="inputBox">
+                        <span>Password</span><br>
+                        <input type="text" name="Password">
+                    </div>
+                    <div class="inputBox">
+                        <input type="submit" name="Submit" value="Confirm changes">
+                    </div>
+                </form>
+    </div>
+</div>
+<div class="edit-modal-appointment">
+    <div class="edit-modal-content">
+        <span class="edit-modal-close-button-appointment">×</span>
+        <form class="admin-edit-form">
+        <i class="fa-solid fa-file-pen"></i>
+        <h2>Currently editing {{Appointment_id}}</h2>
+                    <div class="inputBox">
+                        <span>Appointment date</span><br>
+                        <input type="date" name="date" id="date">
+                    </div>
+                    <div class="inputBox">
+                    <span>Appointment hour</span><br>
+                    <select name="s-select">
+                            <option>Select Hour</option>
+                            <option value="9">9.00</option>
+                            <option value="10">10.00</option>
+                            <option value="11">11.00</option>
+                            <option value="12">12.00</option>
+                            <option value="13">13.00</option>
+                            <option value="14">14.00</option>
+                            <option value="15">15.00</option>
+                            <option value="16">16.00</option>
+                            <option value="17">17.00</option>
+                            <option value="18">18.00</option>
+                            <option value="19">19.00</option>
+                            <option value="20">20.00</option>
+                        </select>
                     </div>
                     <div class="inputBox">
                         <input type="submit" name="Submit" value="Confirm changes">
