73d18f9acf10270cc691d95a6841388763ebd9b1
MINAS\User <ics20072@uom.edu.gr>
2022-11-15 21:37:40 +0200

    Complete API and logic to update patient's non sensitive info

--- src/scripts/services/patientService.php
+++ src/scripts/services/patientService.php
@@ -41,7 +41,6 @@
             }
 
             return $patient;
-   
         }
 
         public function findPatientByLastname($lastname){
@@ -58,7 +57,51 @@
             }
 
             return $patient;
-   
+        }
+
+        public function updatePatientNonSensitiveInfo($patientObj){
+            try {
+                $patientFound = $this->findPatientById($patientObj->id);
+
+                if($patientFound == null){
+                    return "Patient doesn't exist!";
+                }
+    
+                $firstname = $patientFound->firstname;
+                $lastname = $patientFound->lastname;
+                $phone = $patientFound->phone;
+                $location = $patientFound->location;
+                $image = $patientFound->image;
+                
+                if(property_exists($patientObj, 'firstname') && strcmp($patientObj->firstname, "") !== 0 && strcmp($firstname, $patientObj->firstname) !== 0){
+                    $firstname = $patientObj->firstname;
+                }
+
+                if(property_exists($patientObj, 'lastname') && strcmp($patientObj->lastname, "") !== 0 && strcmp($lastname, $patientObj->lastname) !== 0){
+                    $lastname = $patientObj->lastname;
+                }
+                
+                if(property_exists($patientObj, 'phone') && strcmp($patientObj->phone, "") !== 0 && strcmp($phone, $patientObj->phone) !== 0){
+                    $phone = $patientObj->phone;
+                }
+                
+                if(property_exists($patientObj, 'location') && strcmp($location, $patientObj->location) !== 0){
+                    $location = $patientObj->location;
+                }
+                
+                if(property_exists($patientObj, 'image') !== 0 && strcmp($image, $patientObj->image) !== 0){
+                    $image = $patientObj->image;
+                }
+
+                $sql = "UPDATE `$this->table` SET `firstname` = '$firstname', `lastname` = '$lastname', `phone` = '$phone', 
+                                `location` = '$location', `image` = '$image' WHERE `id` = $patientObj->id";
+                $result = $this->mysqli->query($sql);
+                
+                return $this->findPatientById($patientFound->id);
+    
+            }catch(Exception $e){
+                echo 'Message: ' .$e->getMessage();
+            }
         }
     }
 ?>
