934f56a85d1899de91aaa244c1283646d095acf2
MINAS\User <ics20072@uom.edu.gr>
2022-11-18 19:22:18 +0200

    Make links target="_blank" and format the code

--- public/views/our-team.php
+++ public/views/our-team.php
@@ -1,111 +1,110 @@
 <?php
   include '../views/includes/file-begin/file-begin.php';
 ?>
-    <link rel="stylesheet" href="../styles/our-team.css" />
+<link rel="stylesheet" href="../styles/our-team.css" />
 <?php
   include '../views/includes/headers/landing-header.php';
 ?>
-    <section class="welcome">
-        <div class="content">
-            <h2>Our team</h2>
-            <p>This is us and our particular roles in this project!</p>
+<section class="welcome">
+  <div class="content">
+    <h2>Our team</h2>
+    <p>This is us and our particular roles in this project!</p>
+  </div>
+</section>
+<section>
+  <div class="row">
+    <!-- Column 1 -->
+    <div class="column">
+      <div class="card">
+        <div class="img-container">
+          <img src="../resources/images/us/minas.jpg" />
         </div>
-    </section>
-    <section>
-    
-        <div class="row">
-            <!-- Column 1 -->
-            <div class="column">
-                <div class="card">
-                    <div class="img-container">
-                    <img src="../resources/images/us/minas.jpg" />
-                </div>
-                <h3>Charakopoulos Minas-Theodoros</h3>
-                <h5>Backend Development</h5>
-                <p>PHP, SQL, APIs, MVC</p>
-                <div class="icons">
-                    <a href="https://www.linkedin.com/in/minas-theodoros-charakopoulos/" target=blank>
-                    <i class="fa-brands fa-linkedin"></i>
-                    </a>
-                    <a href="https://github.com/ics20044" target=blank>
-                    <i class="fa-brands fa-github"></i>
-                    </a>
-                    <a href="mailto:ics20072@uom.edu.gr" target=blank>
-                    <i class="fa-solid fa-envelope"></i>
-                    </a>
-                </div>
-                </div>
-            </div>
-            <!-- Column 2 -->
-            <div class="column">
-                <div class="card">
-                    <div class="img-container">
-                    <img src="../resources/images/us/dionisis.jpg" />
-                </div>
-                <h3>Lougaris Dionisios</h3>
-                <h5>Frontend Development</h5>
-                <p>HTML5, CSS3, Vanilla Js</p>
-                <div class="icons">
-                    <a href="https://www.linkedin.com/in/dionisis-lougaris/" target=blank>
-                    <i class="fa-brands fa-linkedin"></i>
-                    </a>
-                    <a href="https://github.com/DionisisLougaris" target=blank>
-                    <i class="fa-brands fa-github"></i>
-                    </a>
-                    <a href="mailto:ics20058@uom.edu.gr" target=blank>
-                    <i class="fa-solid fa-envelope"></i>
-                    </a>
-                </div>
-                </div>
-            </div>
+        <h3>Charakopoulos Minas-Theodoros</h3>
+        <h5>Backend Development</h5>
+        <p>PHP, SQL, APIs, MVC</p>
+        <div class="icons">
+          <a href="https://www.linkedin.com/in/minas-theodoros-charakopoulos/" target="_blank">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/ics20072" target="_blank">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a href="mailto:ics20072@uom.edu.gr" target="_blank">
+            <i class="fa-solid fa-envelope"></i>
+          </a>
         </div>
-        <div class="row">
-            <!-- Column 3 -->
-            <div class="column">
-                <div class="card">
-                    <div class="img-container">
-                    <img src="../resources/images/us/panagiotis.png" />
-                </div>
-                <h3>Machairas Panagiotis</h3>
-                <h5>Backend Development</h5>
-                <p>PHP, SQL, APIs</p>
-                <div class="icons">
-                    <a href="https://www.linkedin.com/in/panagiotis-machairas-9263841b9/" target=blank>
-                    <i class="fa-brands fa-linkedin"></i>
-                    </a>
-                    <a href="https://github.com/ics20044" target=blank>
-                    <i class="fa-brands fa-github"></i>
-                    </a>
-                    <a href="mailto:ics20044@uom.edu.gr" target=blank>
-                    <i class="fa-solid fa-envelope"></i>
-                    </a>
-                </div>
-                </div>
-            </div>
-            <!-- Column 4 -->
-            <div class="column">
-                <div class="card">
-                    <div class="img-container">
-                    <img src="../resources/images/us/giorgos.png" />
-                </div>
-                <h3>Stefou Georgios-Ioannis</h3>
-                <h5>Application Architecture</h5>
-                <p>Mockups, UML, App Requirements</p>
-                <div class="icons">
-                    <a href="https://www.linkedin.com/in/george-john-stefou-713a9a1b8/" target=blank>
-                    <i class="fa-brands fa-linkedin"></i>
-                    </a>
-                    <a href="https://github.com/georgestefou" target=blank>
-                    <i class="fa-brands fa-github"></i>
-                    </a>
-                    <a href="mailto:ics20051@uom.edu.gr" target=blank>
-                    <i class="fa-solid fa-envelope"></i>
-                    </a>
-                </div>
-                </div>
-            </div>
+      </div>
+    </div>
+    <!-- Column 2 -->
+    <div class="column">
+      <div class="card">
+        <div class="img-container">
+          <img src="../resources/images/us/dionisis.jpg" />
         </div>
-    </section>
+        <h3>Lougaris Dionisios</h3>
+        <h5>Frontend Development</h5>
+        <p>HTML5, CSS3, Vanilla Js</p>
+        <div class="icons">
+          <a href="https://www.linkedin.com/in/dionisis-lougaris/" target="_blank">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/DionisisLougaris" target="_blank">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a href="mailto:ics20058@uom.edu.gr" target="_blank">
+            <i class="fa-solid fa-envelope"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <!-- Column 3 -->
+    <div class="column">
+      <div class="card">
+        <div class="img-container">
+          <img src="../resources/images/us/panagiotis.png" />
+        </div>
+        <h3>Machairas Panagiotis</h3>
+        <h5>Backend Development</h5>
+        <p>PHP, SQL, APIs</p>
+        <div class="icons">
+          <a href="https://www.linkedin.com/in/panagiotis-machairas-9263841b9/" target="_blank">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/ics20044" target="_blank">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a href="mailto:ics20044@uom.edu.gr" target="_blank">
+            <i class="fa-solid fa-envelope"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- Column 4 -->
+    <div class="column">
+      <div class="card">
+        <div class="img-container">
+          <img src="../resources/images/us/giorgos.png" />
+        </div>
+        <h3>Stefou Georgios-Ioannis</h3>
+        <h5>Application Architecture</h5>
+        <p>Mockups, UML, App Requirements</p>
+        <div class="icons">
+          <a href="https://www.linkedin.com/in/george-john-stefou-713a9a1b8/" target="_blank">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/georgestefou" target="_blank">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a href="mailto:ics20051@uom.edu.gr" target="_blank">
+            <i class="fa-solid fa-envelope"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <?php
   include '../views/includes/footers/landing-footer.php';
 ?>
